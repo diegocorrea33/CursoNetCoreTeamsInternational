d008c1de4425f4d50424a810b30cdb7f1b4f7e7d
diegocorrea <diego.correa33@hotmail.es>
2019-10-19 12:29:28 -0500

    Students implementation

---
+++ TGL/TGL.WebApp/Data/StudentStore.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TGL.WebApp.Models;
+
+namespace TGL.WebApp.Data
+{
+    public class StudentStore
+    {
+        private List<Student> Students { get; set; } = new List<Student>();
+
+        public StudentStore()
+        {
+            Students.Add(new Student
+            {
+                Id = Guid.NewGuid(),
+                Age = 17,
+                Name = "Juan",
+                LastName = "Perez",
+                Nit = "123456789"
+            });
+            Students.Add(new Student
+            {
+                Id = Guid.NewGuid(),
+                Age = 27,
+                Name = "Felipe",
+                LastName = "Orozco",
+                Nit = "987654321"
+            });
+            Students.Add(new Student
+            {
+                Id = Guid.NewGuid(),
+                Age = 14,
+                Name = "Mateo",
+                LastName = "Lotero",
+                Nit = "147852369"
+            });
+        }
+
+        public List<Student> GetStudents() {
+            return this.Students;
+        }
+    }
+}
---
+++ TGL/TGL.WebApp/Pages/Students/Index.cshtml.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TGL.WebApp.Data;
+using TGL.WebApp.Models;
+
+namespace TGL.WebApp.Pages.Students
+{
+    public class IndexModel : PageModel
+    {
+        public StudentStore StudentStore { get; set; }
+        public List<Student> Students { get; set; }
+        public IndexModel(StudentStore studentStore)
+        {
+            StudentStore = studentStore;
+            Students = StudentStore.GetStudents();
+        }
+        public void OnGet()
+        {
+        }
+    }
+}
